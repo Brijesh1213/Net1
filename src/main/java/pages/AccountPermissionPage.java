1c561b47a4aca9552be017ccd8acfc218ea86568
Brijesh Trivedi <brijesh.trivedi@netsweeper.com>
2023-02-06 10:13:20 -0500

    up

--- src/main/java/pages/AccountPermissionPage.java
+++ src/main/java/pages/AccountPermissionPage.java
@@ -43,7 +43,7 @@ public class AccountPermissionPage extends BasePage {
 			  	return getElementforPage(pageTitle);
 		  }	
 	      
-	      //methodss
+	      //method
 	       public void createAdminAccount() throws InterruptedException {
 	    	   driver.switchTo().frame(1);	    	   
 	    	   if(getaccountsTab().isDisplayed()) {
